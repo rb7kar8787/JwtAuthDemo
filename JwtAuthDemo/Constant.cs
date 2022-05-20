4c9982ef676a902ac522cae94162a86603121e5e
rushikesh <rushisatkar9@gmail.com>
2022-05-20 10:05:25 +0530

    Add project files.

---
+++ JwtAuthDemo/Constant.cs
@@ -0,0 +1,9 @@
+﻿namespace JwtAuthDemo
+{
+    public class Constant
+    {
+        public const int JWT_TOKEN_VALIDITY_MINS = 30;
+        public const string JWT_SECURITY_KEY = "Rushikesh_Satkar_12345";
+
+    }
+}
